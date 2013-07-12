6c30a5f15e3b63e7b7c359b58063db2ed3687db6
Martin Zhao <wrzhao1987@163.com>
2013-07-12 13:41:01 +0800

    GrabImage

--- GrabImage.class.php
+++ GrabImage.class.php
@@ -1,2 +1,15 @@
 <?php
 require_once ('Grab.class.php');
+class GrabImage extends Grab
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function fetch($url_list) {}
+
+    public function filter($url_list) {}
+
+    public function save($url_list) {}
+}
